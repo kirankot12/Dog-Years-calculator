1ce5cb69743c37f739f94244e8ad70f7ceb3ac3c
kirankot12 <116199155+kirankot12@users.noreply.github.com>
2022-10-31 23:52:02 +0000

    Add files via upload

---
+++ Dog years calcukator.py
@@ -0,0 +1,12 @@
+#creating a calualtor that takes human years and changes it to dog years 
+
+
+
+HYears = input("input the age of yout dog: ")
+def dogyears (years):
+    years = int(years)
+    return (years*7)
+
+
+print (dogyears(HYears))
+
